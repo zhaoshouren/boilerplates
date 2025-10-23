2802ee06d71ceafff4bfccf44d49f06058ee61a2
Warren Chu <warren.s.chu@live.com>
2025-10-22 19:15:52 -0500

    initializing repo

---
+++ spring-boot/demo/src/test/java/gov/irs/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package gov.irs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
